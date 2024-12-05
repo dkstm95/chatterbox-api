caa67595cad5d5d4e888bbeba5b51cf86a70d543
dkstm95 <dkstm95@naver.com>
2024-12-05 15:57:02 +0900

    feat: topic create

--- src/main/kotlin/topic/domain/Topic.kt
+++ src/main/kotlin/topic/domain/Topic.kt
@@ -1,5 +1,33 @@
 package org.chatterbox.topic.domain
 
-class Topic {
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.Id
+import org.chatterbox.common.BaseTimeEntity
+
+@Entity
+class Topic private constructor(
+    @Id
+    val id: Long?,
+
+    @Column(nullable = false)
+    val userId: Long,
+
+    content: String,
+) : BaseTimeEntity() {
+
+    @Column(nullable = false)
+    var content: String = content
+        protected set
+
+    companion object {
+        fun create(userId: Long, content: String): Topic {
+            return Topic(
+                id = null,
+                userId = userId,
+                content = content,
+            )
+        }
+    }
 
 }
--- src/test/kotlin/topic/domain/TopicTest.kt
+++ src/test/kotlin/topic/domain/TopicTest.kt
@@ -1,12 +1,19 @@
 package topic.domain
 
 import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.shouldBe
 import org.chatterbox.topic.domain.Topic
 
 class TopicTest : FunSpec({
 
-    test("can create a topic") {
-        Topic()
+    test("create") {
+        val userId = 1L
+        val content = "content"
+        val topic = Topic.create(userId, content)
+
+        topic.id shouldBe null
+        topic.userId shouldBe userId
+        topic.content shouldBe content
     }
 
 })
